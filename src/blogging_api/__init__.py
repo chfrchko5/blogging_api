1d141e4e9fdd9bdc4c3571b2bfe63d1b7ece582b
chfrchko5 <r4ger7@gmail.com>
2026-06-01 13:27:57 -0400

    init stuff

--- src/blogging_api/__init__.py
+++ src/blogging_api/__init__.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
 
 def create_app():
-    app = Flask(__name__)
+    load_dotenv()
+    DB_NAME = os.getenv("db_name")
+    DB_USER = os.getenv("db_user")
+    DB_PASS = os.getenv("db_pass")
+    URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@localhost:3306/{DB_NAME}"
 
-    @app.route("/")
-    def home():
-        return "Page Test"
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = URI
+    db = SQLAlchemy(app)
+
+    class Blog(db.Model):
+        __tablename__ = 'blog'
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        title = db.Column(db.String(30), nullable=False)
+        content = db.Column(db.Text, nullable=False)
+        category = db.Column(db.String(20), nullable=False)
+        tags = db.Column(db.Text, nullable=False)
+
+        # DO THESE TWO AFTER SUCCESSFUL TABLE CREATION
+        # createdAt = db.Column(db.TIMESTAMP)
+        # updatedAt = db.Column(db.TIMESTAMP)
+
+    with app.app_context():
+        db.create_all()
 
     return app
